8d9e5c116b33273090384c3df50828ab9bb2d3ab
Mattia <mattia.asti@gmail.com>
2017-05-16 14:46:25 +0100

    fixed exports to allow 'import { RichText } ... '

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import Date from '@root/date';
 import RichText from '@root/richtext';
 
+export {default as Date} from '@root/date';
+export {default as RichText} from '@root/richtext';
+
 export default { Date, RichText };
